0266db4ec8fb0be3e4d3bf9eb2cf2a750e62426b
TomasB22 <t.baeza01@ufromail.cl>
2023-09-26 20:58:24 -0300

    feat: added comments to explain the code

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -7,15 +7,24 @@ import { ApiService } from "./api.service";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
+  /* Lista de clientes recuperados desde la API */
   clientes: any[] = [];
+  /* Lista de clientes morosos filtrados */
   clientesMorosos: any[] = [];
+  /* Cliente seleccionado para edición */
   clienteParaEditar: any = null;
+  /* Cliente seleccionado para eliminar */
   clienteParaEliminar: any = null;
+  /* Lista de clientes a mostrar en la tabla */
   clientesToDisplay: any[] = [];
+  /* Bandera para mostrar u ocultar clientes morosos. */
   mostrarMorososFlag: boolean = false;
+  /* "Bandera" que indica si se están mostrando clientes morosos */
   mostrandoMorosos: boolean = false;
   constructor(private apiService: ApiService) {}
 
+  /* Método que se ejecuta al iniciar el componente */
+  /* Recupera la lista de clientes desde la API y configura la actualización automática de la tabla. */
   ngOnInit() {
     this.apiService.getAllClientes().subscribe((data: any) => {
       this.clientes = data;
@@ -25,13 +34,13 @@ export class AppComponent implements OnInit {
       this.clientesToDisplay = [...this.clientes];
 
       // Llama a la función 'autoActualizarTabla' cada 10 segundos (10000 milisegundos)
-      /* No funciona :() */
       setInterval(() => {
         this.autoActualizarTabla();
       }, 10000);
     });
   }
 
+  /* Actualiza automáticamente la tabla de clientes. */
   autoActualizarTabla() {
     this.apiService.getAllClientes().subscribe(
       (data: any) => {
@@ -52,10 +61,12 @@ export class AppComponent implements OnInit {
     );
   }
 
+  /* Prepara un cliente para la edición */
   editarCliente(cliente: any) {
     this.clienteParaEditar = { ...cliente };
   }
 
+  /* Guarda los cambios realizados en la edición del cliente. */
   guardarEdicion() {
     if (this.clienteParaEditar) {
       this.apiService.updateCliente(this.clienteParaEditar).subscribe(
@@ -69,10 +80,12 @@ export class AppComponent implements OnInit {
     }
   }
 
+  /* Cancela la edición del cliente. */
   cancelarEdicion() {
     this.clienteParaEditar = null;
   }
 
+  /* Confirma la eliminación de un cliente */
   confirmarEliminarCliente(cliente: any) {
     const confirmacion = window.confirm(
       `¿Estás seguro de que deseas eliminar al cliente ${cliente.nombre}?`
@@ -91,7 +104,7 @@ export class AppComponent implements OnInit {
       );
     }
   }
-
+  /* Elimina un cliente por su ID */
   eliminarCliente(clienteId: number) {
     this.apiService.deleteCliente(clienteId).subscribe(
       (response: any) => {
@@ -103,6 +116,7 @@ export class AppComponent implements OnInit {
       }
     );
   }
+  /* Filtra y muestra los clientes morosos. */
   mostrarMorosos() {
     if (this.mostrandoMorosos) {
       this.clientesToDisplay = [...this.clientes];
@@ -114,12 +128,13 @@ export class AppComponent implements OnInit {
       this.mostrandoMorosos = true;
     }
   }
-
+  /* Muestra todos los clientes, incluidos los no morosos. */
   mostrarTodos() {
     this.clientesToDisplay = [...this.clientes];
     this.mostrarMorososFlag = false;
   }
 
+  /* Recarga la lista de clientes desde la API. */
   reloadClientes() {
     this.apiService.getAllClientes().subscribe(
       (data: any) => {
